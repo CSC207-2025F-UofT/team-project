286d36c55b1615131d92322e165e4525ee6da2ef
Lou-Meixuan <meixuan.lou@mail.utoronto.ca>
2025-10-27 14:40:22 -0400

    add the user story

--- src/main/java/Battle_System/User/User.java
+++ src/main/java/Battle_System/User/User.java
@@ -3,4 +3,5 @@ package Battle_System.User;
 public class User {
     //1. As a user I want to have an initial ATK and HP value when the game starts.
     //2. As a user I want to be able to initiate a battle so that I can attack my opponent/monster or choose flee to aviod the fight.
+    //3. As a user I want to be able to access my inventory so that I can equip or use items.
 }
