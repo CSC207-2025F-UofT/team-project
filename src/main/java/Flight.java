cd5bdc38d9b8ad6983de5f32d74a8f32b67394ab
Macqueen <macqueenpan@gmail.com>
2025-11-03 10:23:24 -0500

    Create class Flight

---
+++ src/main/java/Flight.java
@@ -0,0 +1,18 @@
+public class Flight {
+    private String flightNumber;
+    private String departureAirport;
+    private String arrivalAirport;
+    private String status;
+    private String departureTime;
+    private String arrivalTime;
+
+    public Flight(String flightNumber, String departureAirport, String arrivalAirport,
+                  String status, String departureTime, String arrivalTime) {
+        this.flightNumber = flightNumber;
+        this.departureAirport = departureAirport;
+        this.arrivalAirport = arrivalAirport;
+        this.status = status;
+        this.departureTime = departureTime;
+        this.arrivalTime = arrivalTime;
+    }
+}
