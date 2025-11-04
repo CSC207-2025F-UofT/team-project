1828c1cf50410b8f15197351a6d9dc38f92076e2
Aiden <aiden.chettleburgh720@gmail.com>
2025-11-04 12:52:37 -0500

    Added barebones project file structure

---
+++ src/main/java/app/AppBuilder.java
@@ -0,0 +1,21 @@
+package app;
+
+public class AppBuilder {
+    // TODO: Add instance variables
+
+    public AppBuilder() {
+        // TODO: Implement Constructor
+    }
+
+    // TODO: Implement builder methods
+
+    // Here's an example method from the CA Lab:
+    /*
+    public AppBuilder addLoginView() {
+        loginViewModel = new LoginViewModel();
+        loginView = new LoginView(loginViewModel);
+        cardPanel.add(loginView, loginView.getViewName());
+        return this;
+    }
+     */
+}
