62a1b3e1fcc682e8641b47433318f7e68d1dde7d
gosha003 <igortatarov06@gmail.com>
2025-11-03 23:52:49 -0500

    Removed unnecessary comments

--- src/main/java/entity/BookmarkedLocation.java
+++ src/main/java/entity/BookmarkedLocation.java
@@ -2,7 +2,6 @@ package entity;
 
 import java.time.Instant;
 
-// 'BookmarkedLocation' Entity class which extends 'Location' Entity class.
 public class BookmarkedLocation extends entity.Location {
     private final String name;
     private final Instant savedTime;
@@ -12,11 +11,11 @@ public class BookmarkedLocation extends entity.Location {
         this.name = name;
         this.savedTime = Instant.now();
     }
-// Getter for the private attribute 'name'.
+
     public String getName() {
         return name;
     }
-// Getter for the private attribute 'savedTime'.
+
     public Instant getSavedTime() {
         return savedTime;
     }
