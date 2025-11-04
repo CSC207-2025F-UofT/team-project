3d3120b4e61751686f508632ab7ab5555955f784
cheu1801 <cotaroy.cheung@mail.utoronto.ca>
2025-11-04 12:20:15 -0500

    dependencies

--- src/main/java/entity/Move.java
+++ src/main/java/entity/Move.java
@@ -1,4 +1,19 @@
 package entity;
 
 public class Move {
+    private String name;
+    private int power;
+    private int accuracy;
+    private String effect;
+    private int type_id;
+    private int damage;
+
+    public Move(String name, int power, int accuracy, String effect, int type_id, int damage) {
+        this.name = name;
+        this.power = power;
+        this.accuracy = accuracy;
+        this.effect = effect;
+        this.type_id = type_id;
+        this.damage = damage;
+    }
 }
--- src/main/java/entity/Type.java
+++ src/main/java/entity/Type.java
@@ -4,17 +4,23 @@ import java.util.List;
 
 public class Type {
     private String name;
+    private int id;
     private List<String> strengths;
     private List<String> weaknesses;
 
-    public Type(String name, List<String> strengths, List<String> weaknesses) {
+    public Type(String name, int id, List<String> strengths, List<String> weaknesses) {
         this.name = name;
+        this.id = id;
         this.strengths = strengths;
         this.weaknesses = weaknesses;
     }
 
     public String getName() {
         return name;
+    }
+
+    public int  getId() {
+        return id;
     }
 
     public List<String> getStrengths() {
