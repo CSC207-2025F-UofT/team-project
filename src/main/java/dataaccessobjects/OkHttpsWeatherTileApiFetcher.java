480d83ac8677bbf8f25b6f97596330ad9d011932
kwanyinlai <laikwanyin06@gmail.com>
2025-11-06 15:26:43 -0500

    implemented basic image retrieval

--- src/main/java/dataaccessobjects/OkHttpsWeatherTileApiFetcher.java
+++ src/main/java/dataaccessobjects/OkHttpsWeatherTileApiFetcher.java
@@ -1,10 +1,14 @@
 package dataaccessobjects;
 
 import dataaccessinterface.WeatherTileApiFetcher;
+
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import entity.WeatherTile;
 import entity.WeatherType;
 import okhttp3.OkHttpClient;
+
+import javax.imageio.ImageIO;
 
 public class OkHttpsWeatherTileApiFetcher implements WeatherTileApiFetcher {
     private final OkHttpClient client = new OkHttpClient();
@@ -12,29 +16,32 @@ public class OkHttpsWeatherTileApiFetcher implements WeatherTileApiFetcher {
     public BufferedImage getWeatherTileImageData(WeatherTile tile) throws TileNotFoundException {
         String url = "https://weathermaps.weatherapi.com/";
         final Request request = new Request.Builder()
-                .url(String.format("%s/tiles/%s/%s/%s/%s/%s.png",
+                .url(String.format("%s/%s/tiles/%s/%s/%s/%s/%s.png",
                         url,
                         tile.getWeatherType().name().toLowerCase(),
                         tile.getTimestamp(),
                         tile.getTimestamp(),
-                        tile.getCoordinates().getX(),
-                        tile.getCoordinates().getY(),
-                        tile.getCoordinates().getZoom()))
+                        tile.getCoordinates().x,
+                        tile.getCoordinates().y,
+                        tile.getCoordinates().zoom))
                 .addHeader("Content-Type", "application/json")
                 .build();
 
         try {
             final Response response = client.newCall(request).execute();
-            final JSONObject responseBody = new JSONObject(response.body().string());
-
-            if (responseBody.getString("status").equals("success")) {
-                final JSONArray subbreeds = responseBody.getJSONArray("message");
-                for (var breedName : subbreeds) {
-                    subbreedList.add(breedName.toString());
-                }
+            if (response.code() == 404) {
+                throws new IOException();
             }
-            else {
-                throw new BreedFetcher.BreedNotFoundException("Subbreeds could not be found for " + breed);
+            try {
+                InputStream in = response.body().byteStream();
+                BufferedImage image = ImageIO.read(in);
+                if (image == null){
+                    throws new IOException();
+                }
+                return image;
+            }
+            catch (IOException){
+                throws new IOException();
             }
         } catch (IOException e) {
             throw new BreedFetcher.BreedNotFoundException("Subbreeds could not be found for " + breed);
