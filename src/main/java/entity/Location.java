e6c0994c02ccb5470e5c9d148880260e621d1574
griffin12 <wangyanlin0712@gmail.com>
2025-11-05 23:02:04 -0500

    updated Location

--- src/main/java/entity/Location.java
+++ src/main/java/entity/Location.java
@@ -16,4 +16,12 @@ public class Location {
     public double getLongitude() {
         return longitude;
     }
-}
+
+    public TileCoords getTileCoords(int zoom) {
+        double n = 1 << zoom;
+        double latRad = Math.toRadians(latitude);
+        int x = (int) Math.floor((longitude + 180.0) / 360.0 * n);
+        int y = (int) Math.floor((1 - Math.log(Math.tan(latRad) + 1 / Math.cos(latRad)) / Math.PI) / 2 * n);
+        return new TileCoords(x, y, zoom);
+    }
+ }
