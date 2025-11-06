b7f2ef09004a9e2bbe352cb7275097823a42874d
Macqueen <macqueenpan@gmail.com>
2025-11-05 23:29:51 -0500

    Implement entity Favourite

---
+++ src/main/java/Entities/Favourite.java
@@ -0,0 +1,34 @@
+package Entities;
+
+public class Favourite {
+
+    private final String userId;
+    private final String flightNumber;
+
+    public Favourite(String userId, String flightNumber) {
+        this.userId = userId;
+        this.flightNumber = flightNumber;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Favourite favourite = (Favourite) obj;
+        return userId.equals(favourite.userId) &&
+                flightNumber.equals(favourite.flightNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return userId.hashCode() + flightNumber.hashCode();
+    }
+}
