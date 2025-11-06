1bfff2275ed049b38dc7e7aceed8181d833e224f
kwanyinlai <laikwanyin06@gmail.com>
2025-11-06 15:13:39 -0500

    add WeatherType attribute to weathertiles swith constructor and getter

--- src/main/java/entity/WeatherTile.java
+++ src/main/java/entity/WeatherTile.java
@@ -3,16 +3,22 @@ package entity;
 public class WeatherTile {
     private final TileCoords coordinates;
     private final java.time.Instant timestamp;
+    private final WeatherType weatherType;
 
-    public WeatherTile(TileCoords coordinates, java.time.Instant timestamp) {
+    public WeatherTile(TileCoords coordinates, java.time.Instant timestamp, WeatherType weatherType) {
         this.coordinates = coordinates;
         this.timestamp = timestamp;
+        this.weatherType = weatherType;
     }
 
     public java.time.Instant getTimestamp() { return timestamp; }
 
     public TileCoords getCoordinates() {
         return coordinates;
+    }
+
+    public WeatherType getWeatherType() {
+        return weatherType;
     }
 
     public String generateKey(){
