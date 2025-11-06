c609d101f257e709aa80f23ff57da3ef5bc122a4
raph <rafwing100@outlook.com>
2025-11-06 14:18:54 -0500

    Add files via upload

---
+++ src/main/java/use_case/login/LoginInputBoundary.java
@@ -0,0 +1,21 @@
+package use_case.login;
+
+/**
+ * Input Boundary for actions which are related to logging in.
+ */
+public interface LoginInputBoundary {
+
+    /**
+     * Executes the login use case.
+     *
+     * @param loginInputData the input data
+     */
+    void execute(LoginInputData loginInputData);
+
+
+    /**
+     * Executes the switch to sign up  view use case.
+     */
+    void switchToSignUpView();
+
+}
