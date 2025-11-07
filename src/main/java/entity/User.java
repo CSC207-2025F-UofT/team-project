d2f17cc32a9b198e5c9837c8459a6ec7fc13bf24
AquaDummy <awesomejay098@gmail.com>
2025-11-06 19:21:48 -0500

    Added user entity

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -1,4 +1,24 @@
 package entity;
+import java.util.ArrayList;
 
 public class User {
+    private final String name;
+    private final String password;
+    private final float[] coords = new float[2];
+
+    public User(String name, String password){
+        this.name = name;
+        this.password = password;
+        this.coords[0] = 0f;
+        this.coords[1] = 0f;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public String getPassword(){
+        return this.password;
+    }
+    public float[] getCoords(){
+        return this.coords;
+    }
 }
