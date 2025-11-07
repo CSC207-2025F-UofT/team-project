1b18b5fb6f10327a1b4cf55b6c5eb69a5385119f
BaihanW <baihan.wu@mail.utoronto.ca>
2025-11-07 00:05:46 -0500

    Deal with different exceptions inside scope

--- src/main/java/use_case/search/SearchInteractor.java
+++ src/main/java/use_case/search/SearchInteractor.java
@@ -2,6 +2,8 @@ package use_case.search;
 
 import entity.Location;
 
+import java.io.IOException;
+
 /**
  * The Search Interactor.
  */
@@ -16,18 +18,27 @@ public class SearchInteractor implements SearchInputBoundary {
     }
 
     @Override
-    public void execute(SearchInputData searchInputData) throws Exception {
+    public void execute(SearchInputData searchInputData) {
         final String locationName = searchInputData.getLocationName();
-        if (!searchDataAccessObj.existsByName(locationName)) {
-            searchPresenter.prepareFailView(locationName + ": Location does not exist.");
-        }
-        else {
-            // get location data from OSM
-            final Location location = searchDataAccessObj.get(searchInputData.getLocationName());
+        try {
+            if (!searchDataAccessObj.existsByName(locationName)) {
+                searchPresenter.prepareFailView(locationName + ": Location does not exist.");
+            }
+            else {
+                // get location data from OSM
+                final Location location = searchDataAccessObj.get(searchInputData.getLocationName());
 
-            final SearchOutputData searchOutputData = new SearchOutputData(location.getName(),
-                    location.getLatitude(), location.getLongitude());
-            searchPresenter.prepareSuccessView(searchOutputData);
+                final SearchOutputData searchOutputData = new SearchOutputData(location.getName(),
+                        location.getLatitude(), location.getLongitude());
+                searchPresenter.prepareSuccessView(searchOutputData);
+            }
+        } catch (IOException e) {
+            searchPresenter.prepareFailView("Network error while searching for location: " + e.getMessage());
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            searchPresenter.prepareFailView("Request interrupted while searching for location.");
+        } catch (Exception e) {
+            searchPresenter.prepareFailView("Unexpected error: " + e.getMessage());
         }
     }
 }
