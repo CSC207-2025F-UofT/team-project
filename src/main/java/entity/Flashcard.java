4a7ea662fa85ffb113b9b2dcceca36b3b32499a5
hello <hellojennifer.du@mail.utoronto.ca>
2025-11-09 17:18:13 -0500

    flashcard entity

--- src/main/java/entity/Flashcard.java
+++ src/main/java/entity/Flashcard.java
@@ -1,4 +1,35 @@
 package entity;
 
 public class Flashcard {
+    private String sourceWord;
+    private String targetWord;
+    private boolean known;
+    private int deckId;
+
+    public Flashcard(String sourceWord, String targetWord, int deckId) {
+        this.sourceWord = sourceWord;
+        this.targetWord = targetWord;
+        this.deckId = deckId;
+        this.known = false;
+    }
+
+    public String getSourceWord() {
+        return sourceWord;
+    }
+
+    public String getTargetWord() {
+        return targetWord;
+    }
+
+    public void setKnown() {
+        this.known = true;
+    }
+
+    public boolean isKnown() {
+        return this.known;
+    }
+
+    public int getDeckId() {
+        return deckId;
+    }
 }
