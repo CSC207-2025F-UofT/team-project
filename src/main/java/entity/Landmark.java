fec37215267a93bd88547895254cc09ce67e12ba
joeylai96 <joeylai249@gmail.com>
2025-11-10 14:42:04 -0500

    basic structure for Note, Visit, User entities.

--- src/main/java/entity/Landmark.java
+++ src/main/java/entity/Landmark.java
@@ -1,4 +1,18 @@
 package entity;
 
 public class Landmark {
+//    private final String landmarkId;
+//    private final String landmarkName;
+//    private final LandmarkType landmarkType;
+//    private final String landmarkDescription;
+//    private final String address;
+//    private final Double latitude;
+//    private final Double longitude;
+//    private final String imageUrl;
+//    private final String openHours;
+//    private final int totalVisitCount;
+//
+//    public Landmark(String landmarkId, String landmarkName, Landmark landmarkType, String landmarkDescription, String address, double latitude, double longitude, String imageUrl, String openHours, int totalVisitCount) {
+//
+//    }
 }
--- src/main/java/entity/Visit.java
+++ src/main/java/entity/Visit.java
@@ -1,4 +1,26 @@
 package entity;
 
+import java.time.Instant;
+import java.util.List;
+
 public class Visit {
+    private final String visitId;
+    private final Landmark landmark;
+    private final Instant visitedAt;
+
+    public Visit(String visitId,  Landmark landmark) {
+        this.visitId = visitId;
+        this.landmark = landmark;
+        this.visitedAt =  Instant.now();
+    }
+    public String getVisitId() {
+        return visitId;
+    }
+    public Landmark getLandmark() {
+        return landmark;
+    }
+    public Instant getVisitedAt() {
+        return visitedAt;
+    }
+
 }
