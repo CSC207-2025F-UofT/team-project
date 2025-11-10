10896b040900501528a16795662293894b6f8cd6
Raihaan Sandhu <63803872+GaminRick7@users.noreply.github.com>
2025-11-10 15:39:00 -0500

    fix: remove goal from category

--- src/main/java/entity/Category.java
+++ src/main/java/entity/Category.java
@@ -4,29 +4,18 @@ package entity;
 public class Category {
 
     private String name;
-    private double monthlyGoal; // optional, 0 if not set
 
-    public Category(String name, String description, double monthlyGoal) {
+    public Category(String name, String description) {
         this.name = name;
-        this.monthlyGoal = monthlyGoal;
     }
 
     public String getName() {
         return name;
     }
 
-    public double getMonthlyGoal() {
-        return monthlyGoal;
-    }
 
     public void rename(String newName) {
         this.name = newName;
     }
 
-    public void updateMonthlyGoal(double newGoalAmount) {
-        if (newGoalAmount < 0) {
-            throw new IllegalArgumentException("Monthly goal cannot be negative.");
-        }
-        this.monthlyGoal = newGoalAmount;
-    }
 }
