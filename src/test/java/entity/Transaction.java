88701e733a6b7806ceab0678ae4ff2dd3418fe69
yunji4 <yunji5v@gmail.com>
2025-11-10 15:38:09 -0500

    fixed all the transaction edits

--- src/test/java/entity/Transaction.java
+++ src/test/java/entity/Transaction.java
@@ -1,44 +1,42 @@
 package entity;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 
 public class Transaction {
-    private final int date;
+    private final LocalDate date;
     private final String  source;
     private final double amount;
     private String category;
 
 
     //create transaction class with category
-    public Transaction(String date, String source, double amount){
-        this.date = get_date(date);
+    public Transaction(String source, double amount, LocalDate date, String category){
+        this.date = date;
         this.source = source;
         this.amount =  amount;
-        this.category =  get_category(source);
+        this.category = source;
 
 
     }
 
-    public String get_category(String source){
 
-        //TODO: fetching the category from the database
 
-        return "category";
-
+    public double getAmount(){
+        return this.amount;
     }
 
-    public int get_date(String date){
-
-        //TODO: getting the month and date as a integer- 11/10/2024 = 11102024
-
-        //Assumes dates is in MM/DD/YYYY format
-        String clean_date = "";
-        for (int i = 0; i< date.length(); i++){
-            if (date.charAt(i) != '/') {
-                clean_date += date.charAt(i);
-            }
-        }
-
-        return Integer.parseInt(clean_date);
-
+    public String getCategory(){
+        return this.category;
     }
+
+    public String getSource(){
+        return this.source;
+    }
+
+    public LocalDate getDate(){
+        return this.date;
+    }
+
 
 }
