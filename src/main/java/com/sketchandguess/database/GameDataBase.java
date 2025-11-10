4de2fdd3726c1e113003f20db4598f228095de45
ponsiclius <davidparent64@gmail.com>
2025-11-10 14:49:25 -0500

    outlined gamedatabase class

--- src/main/java/com/sketchandguess/database/GameDataBase.java
+++ src/main/java/com/sketchandguess/database/GameDataBase.java
@@ -1,4 +1,58 @@
 package com.sketchandguess.database;
 
+import com.sketchandguess.entities.GameRecord;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
 public class GameDataBase implements DataBase {
+    // The database storing each game.
+    // The string contains the prompt used for the game, and helps us search for specific prompts.
+    public HashMap<Integer, GameRecord> GameData;
+
+    public GameDataBase() {
+        this.GameData = new HashMap<>();
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(getClass()
+                    .getClassLoader().getResource(games.csv).toURI()));
+
+            Iterator<String> iterator = lines.iterator();
+            while (iterator.hasNext()) {
+                String line = iterator.next();
+                String[] parts = line.split("|");
+                GameRecord currentGame = new GameRecord(); // TODO: implement this once gameRecord constructor is implemented
+                GameData.put(Integer.parseInt(parts[0]), currentGame);
+            }
+        }
+        catch (IOException | URISyntaxException ex) {
+            throw new RuntimeException(ex);
+        }
+        ;
+    }
+
+    // overloaded version for search function
+    public GameDataBase(HashMap<Integer, GameRecord> GameData) {
+        this.GameData = GameData;
+    }
+
+    public GameRecord GetGame(Integer GameCode) {
+        return GameData.get(GameCode);
+    }
+
+    public GameDataBase SearchWord(GameDataBase DB, String Query) {
+        HashMap<Integer, GameRecord> Matches = new HashMap<>();
+        for (GameRecord g: DB.GameData.values()) {
+            // TODO come back to this once gamerecord is implemented
+            if (g.getPrompt().contains(Query)) {
+                Matches.put(Matches.size(), g);
+            }
+        }
+        GameDataBase MatchBase = new GameDataBase(Matches);
+        return MatchBase;
+    }
 }
