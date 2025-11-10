32210e9f32a1c85a78df385f582e1a81d5af6af1
abdullah2056 <abdullah_2056@hotmail.com>
2025-11-10 14:09:45 -0500

    created User class entity

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -1,1 +1,24 @@
+package entity;
 
+import java.util.Date;
+import java.util.List;
+
+public class User {
+    private String username;
+    private String password:
+    private ArrayList<Recipe> publishedRecipes;
+    private ArrayList<Recipe> savedRecipes;
+    priavte ArrayList<Review> reviews;
+
+    public List<Recipe> getpublishedRecipes() {
+        return publishedRecipes;
+    }
+
+    public List<Recipe> getsavedRecipes() {
+        return savedRecipes;
+    }
+
+    public List<Review> getpublishedReviews() {
+        return reviews;
+    }
+}
