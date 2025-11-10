6b6c72845d0c22edd309e2c3682b13d7ec2ad7fe
albertkang1004 <albertkang1004@gmail.com>
2025-11-10 16:18:01 -0500

    editing question class

--- src/main/java/entity/Question.java
+++ src/main/java/entity/Question.java
@@ -1,5 +1,14 @@
 package entity;
 
 public class Question {
+    public final String questionText;
+    public final String answerText;
+    public final int difficultyLevel;
+    public Question(String questionText, String answerText, int difficultyLevel) {
+        this.questionText = questionText;
+        this.answerText = answerText;
+        this.difficultyLevel = difficultyLevel;
+    }
+
 
 }
