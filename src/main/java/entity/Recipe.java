d6e6ec084581af32fa146b16d2ddac5ca0092492
Oliver Kwun-Morfitt <oliverkwunmorfitt@gmail.com>
2025-11-10 13:30:07 -0500

    added intial recipe entity

--- src/main/java/entity/Recipe.java
+++ src/main/java/entity/Recipe.java
@@ -1,1 +1,27 @@
+package entity;
 
+import java.util.Date;
+import java.util.List;
+
+public class Recipe {
+
+    private String recipeName;
+    private int recipeId;
+    private String description;
+    private List<Ingredient> ingredients;
+    private String category;
+    private boolean shareable;
+    private Date creationDate;
+    private Date updateDate;
+    private int views;
+    private int saves;
+    private List<Review> reviews;
+
+    public String getRecipeName() {
+        return recipeName;
+    }
+    public void setRecipeName(String recipeName) {
+        this.recipeName = recipeName;
+    }
+
+}
