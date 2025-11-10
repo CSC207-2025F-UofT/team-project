2869c115c23bf83b303a23e0f966a437210c9b5b
Toadytop <tom.philip.ca@gmail.com>
2025-11-10 17:42:07 -0500

    Add properties to User and implement credential validation.

--- src/main/java/com/studyarc/entity/User.java
+++ src/main/java/com/studyarc/entity/User.java
@@ -1,4 +1,17 @@
 package com.studyarc.entity;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class User {
+    private byte[] salt;
+    private byte[] passwordHash;
+
+    public boolean validateHash(String password) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        md.update(password.getBytes(StandardCharsets.UTF_8));
+        md.update(this.salt);
+        return md.digest()==this.passwordHash;
+    }
 }
