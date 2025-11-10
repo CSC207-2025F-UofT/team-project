e0fa16ab1749d9965cf4a598aa273f7223f939dc
cheu1801 <cotaroy.cheung@mail.utoronto.ca>
2025-11-10 18:02:25 -0500

    InMemoryUserDataAccessObject for testing

--- src/main/java/data_access/InMemoryUserDataAccessObject.java
+++ src/main/java/data_access/InMemoryUserDataAccessObject.java
@@ -1,4 +1,21 @@
 package data_access;
 
-public class InMemoryDataAccessObject {
+import entity.Team;
+import use_case.grade_team.GradeTeamUserDataAccessInterface;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class InMemoryUserDataAccessObject implements GradeTeamUserDataAccessInterface {
+
+    private final Map<String, Team> teams = new HashMap<>();
+
+    public void saveTeam(Team team) {
+        teams.put(team.getTeamName(), team);
+    }
+
+    @Override
+    public Team getTeam(String teamName) {
+        return teams.get(teamName);
+    }
 }
