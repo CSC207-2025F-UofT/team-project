2aef0806b11b6318babe8259af91c8dce4b95266
cheu1801 <cotaroy.cheung@mail.utoronto.ca>
2025-11-10 17:39:28 -0500

    some more test cases

--- src/test/java/use_case/grade_team/GradeTeamInteractorTest.java
+++ src/test/java/use_case/grade_team/GradeTeamInteractorTest.java
@@ -1,6 +1,7 @@
 package use_case.grade_team;
 
 import entity.GradingStrategy;
+import entity.Pokemon;
 import entity.Team;
 import org.junit.jupiter.api.Test;
 
@@ -30,4 +31,53 @@ class GradeTeamInteractorTest {
         interactor.execute(inputData);
     }
 
+    @Test
+    void fullTeamTest(){
+        Team team = new Team("fullTeam");
+        for(int i = 0; i < 6; i++){
+            team.setPokemon(new Pokemon(), i);
+        }
+        GradingStrategy strategy = new TestStrategy();
+        GradeTeamInputData inputData = new GradeTeamInputData(team, strategy);
+
+        GradeTeamOutputBoundary successPresenter = new GradeTeamOutputBoundary(){
+            @Override
+            public void prepareSuccessView(GradeTeamOutputData gradeTeamOutputData) {
+                assertEquals(6., gradeTeamOutputData.getTeamScore());
+            }
+
+            @Override
+            public void prepareFailureView(String errorMessage) {
+                fail("Use Case failure is unexpected.");
+            }
+        };
+
+        GradeTeamInputBoundary interactor = new GradeTeamInteractor(inputData, successPresenter);
+        interactor.execute(inputData);
+    }
+
+    @Test
+    void firstHalfTeamTest(){
+        Team team = new Team("fullTeam");
+        for(int i = 0; i < 3; i++){
+            team.setPokemon(new Pokemon(), i);
+        }
+        GradingStrategy strategy = new TestStrategy();
+        GradeTeamInputData inputData = new GradeTeamInputData(team, strategy);
+
+        GradeTeamOutputBoundary successPresenter = new GradeTeamOutputBoundary(){
+            @Override
+            public void prepareSuccessView(GradeTeamOutputData gradeTeamOutputData) {
+                assertEquals(3., gradeTeamOutputData.getTeamScore());
+            }
+
+            @Override
+            public void prepareFailureView(String errorMessage) {
+                fail("Use Case failure is unexpected.");
+            }
+        };
+
+        GradeTeamInputBoundary interactor = new GradeTeamInteractor(inputData, successPresenter);
+        interactor.execute(inputData);
+    }
 }
