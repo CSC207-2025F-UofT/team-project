766bc4a92281abe2b522b8c578db53b2a720f8dc
albertkang1004 <albertkang1004@gmail.com>
2025-11-10 16:36:55 -0500

    merging with mahir

--- src/main/java/entity/Question.java
+++ src/main/java/entity/Question.java
@@ -1,14 +1,12 @@
 package entity;
 
-public class Question {
+public abstract class Question {
     public final String questionText;
-    public final String answerText;
     public final int difficultyLevel;
-    public Question(String questionText, String answerText, int difficultyLevel) {
+
+    public Question(String questionText, int difficultyLevel) {
         this.questionText = questionText;
-        this.answerText = answerText;
         this.difficultyLevel = difficultyLevel;
     }
-
 
 }
