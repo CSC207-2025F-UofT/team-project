c786eaa90eaa471acf838fc0cec1718f8b5df9b8
Gezhe14 <61716813+Gezhe14@users.noreply.github.com>
2025-11-10 14:12:19 -0500

    Added variables

--- src/main/java/entity/SharedRecipe.java
+++ src/main/java/entity/SharedRecipe.java
@@ -1,1 +1,9 @@
+package entity;
+import java.util.Date;
 
+public class SharedRecipe {
+    private int shareId;
+    private int recipeId;
+    private int sharedByUserId;
+    private Date dateShare;
+}
