795d70e771bd4f1c15d0b51e3a35734fafde4bb6
stingythrower <maoladina@gmail.com>
2025-11-10 15:43:12 -0500

    Completed LearningGoal entity

--- src/main/java/entity/LearningGoal.java
+++ src/main/java/entity/LearningGoal.java
@@ -1,4 +1,24 @@
 package entity;
+import java.time.LocalDate;
 
 public class LearningGoal {
+    private final int id;
+    private final int userId;
+    private int dailyTarget;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    public LearningGoal(int id, int userId, int dailyTarget, LocalDate startDate, LocalDate endDate) {
+        this.id = id;
+        this.userId = userId;
+        this.dailyTarget = dailyTarget;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public int getId() { return id;}
+    public int getUserId() { return userId;}
+    public int getDailyTarget() { return dailyTarget;}
+    public LocalDate getStartDate() { return startDate;}
+    public LocalDate getEndDate() { return endDate;}
 }
