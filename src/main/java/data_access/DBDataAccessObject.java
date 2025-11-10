7f35a3aaca7bd18f23db632f1cefe53659f7b219
Hugo Ye <hugoye122@gmail.com>
2025-11-10 17:01:37 -0500

    implemented basic api calling in db, check for errors in api_key addage

--- src/main/java/data_access/DBDataAccessObject.java
+++ src/main/java/data_access/DBDataAccessObject.java
@@ -8,10 +8,21 @@ public class DBDataAccessObject {
     private static final String BASE_URL = "https://app.ticketmaster.com/discovery/v2/events.json";
     private static final String API_KEY = System.getenv("DB_KEY");
 
-    public String[] get() {
+    public void get() {
+        System.out.println("Getting data from DB");
         final OkHttpClient client = new OkHttpClient().newBuilder().build();
         HttpUrl.Builder urlBuilder = HttpUrl.parse(BASE_URL).newBuilder();
-
-        return new String[] {};
+        urlBuilder.addQueryParameter("apikey", API_KEY);
+        final Request request = new Request.Builder().url(urlBuilder.build()).build();
+        try {
+            final Response response = client.newCall(request).execute();
+            System.out.println(response.code());
+            if (response.isSuccessful()) {
+                System.out.println("success");
+                System.out.println(response.body().string());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
