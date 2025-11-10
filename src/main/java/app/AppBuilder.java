4c7db5fd5b500f19e796f61b975353dfac5b77a6
Adarsh Padalia <adarshkpadalia@gmail.com>
2025-11-10 13:22:11 -0500

    Initial app changes

--- src/main/java/app/AppBuilder.java
+++ src/main/java/app/AppBuilder.java
@@ -1,4 +1,12 @@
 package app;
 
+import javax.swing.*;
+
 public class AppBuilder {
+    public JFrame build() {
+        final JFrame application = new JFrame("Recipe Application");
+        application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        return application;
+    }
 }
