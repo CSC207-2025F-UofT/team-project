5c7eb8441957cdd90613d3dd24f0988e7486ce7a
NathanFermin <nathan.fermin@mail.utoronto.ca>
2025-11-11 11:17:23 -0500

    Pokemone Lookup Case Now Works minus pokedexes

--- src/main/java/use_case/BuildPokemonTeam/BuildPokemonTeamInteractor.java
+++ src/main/java/use_case/BuildPokemonTeam/BuildPokemonTeamInteractor.java
@@ -10,6 +10,7 @@ import entity.Pokemon;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class BuildPokemonTeamInteractor implements BuildPokemonTeamInputBoundary {
@@ -62,80 +63,108 @@ public class BuildPokemonTeamInteractor implements BuildPokemonTeamInputBoundary
 
                     String pokename = json.getString("name");
                     System.out.println(pokename);
-                    System.out.println(json);
+//                    System.out.println(json);
+//                    System.out.println(json.getJSONArray("types").getJSONObject(0).getJSONObject("type").getString("url"));
+
                     String ptype1 = json.getJSONArray("types").getJSONObject(0).getJSONObject("type").getString("url");
+                    System.out.println(ptype1);
 
                     String[] atype1 = ptype1.split("/");
-                    int type1 = Integer.parseInt(atype1[atype1.length - 2]);
-                    String ptype2 = json.getJSONArray("types").getJSONObject(1).getJSONObject("type").getString("url");
-                    String[] atype2 = ptype2.split("/");
-                    int type2 = Integer.parseInt(atype2[atype2.length - 2]);
+                    System.out.println(Arrays.toString(atype1));
+
+                    int x = atype1.length;
+                    System.out.println(x);
+
+                    int type1 = Integer.parseInt(atype1[atype1.length - 1]);
+
+                    System.out.println(type1 + "\n");
+
+                    // Ok so here dani wants the if statement
+
+                    int type2 = 0;
+
+                    if (ptype1.length() == 2) {
+                        String ptype2 = json.getJSONArray("types").getJSONObject(1).getJSONObject("type").getString("url");
+
+                        String[] atype2 = ptype2.split("/");
+                        type2 = Integer.parseInt(atype2[atype2.length - 1]);
+
+
+                    }
                     ArrayList<Integer> stats = new ArrayList<Integer>();
+
                     for (int count = 0; count < 6; count++) {
                         stats.add(json.getJSONArray("stats").getJSONObject(count).getInt("base_stat"));
                     };
+
+
                     ArrayList<Integer> abilities = new ArrayList<Integer>();
                     int hidden = 0;
+
+
                     // a pokemon has 1 regular ability
                     if (json.getJSONArray("abilities").length() == 1) {
                         String pabil = json.getJSONArray("abilities").getJSONObject(0).getJSONObject("ability").getString("url");
                         String[] aabil = pabil.split("/");
-                        int abil = Integer.parseInt(aabil[aabil.length - 2]);
+                        int abil = Integer.parseInt(aabil[aabil.length - 1]);
                         abilities.add(abil);
                     }
                     // a pokemon has 1 hidden ability and 1 regular ability
                     if (json.getJSONArray("abilities").length() == 2 && json.getJSONArray("abilities").getJSONObject(1).getInt("slot") == 3) {
                         String pabil = json.getJSONArray("abilities").getJSONObject(0).getJSONObject("ability").getString("url");
                         String[] aabil = pabil.split("/");
-                        int abil = Integer.parseInt(aabil[aabil.length - 2]);
+                        int abil = Integer.parseInt(aabil[aabil.length - 1]);
                         abilities.add(abil);
                     }
                     // a pokemon has two regular abilities and no hidden ability
                     if (json.getJSONArray("abilities").length() == 2 && json.getJSONArray("abilities").getJSONObject(1).getInt("slot") == 2) {
                         String pabil = json.getJSONArray("abilities").getJSONObject(0).getJSONObject("ability").getString("url");
                         String[] aabil = pabil.split("/");
-                        int abil = Integer.parseInt(aabil[aabil.length - 2]);
+                        int abil = Integer.parseInt(aabil[aabil.length - 1]);
                         abilities.add(abil);
                         String pabil2 = json.getJSONArray("abilities").getJSONObject(1).getJSONObject("ability").getString("url");
                         String[] aabil2 = pabil2.split("/");
-                        int abil2 = Integer.parseInt(aabil2[aabil2.length - 2]);
+                        int abil2 = Integer.parseInt(aabil2[aabil2.length - 1]);
                         abilities.add(abil2);
                     }
                     // a pokemon has all regular and hidden abilities
                     if (json.getJSONArray("abilities").length() == 3) {
                         String pabil = json.getJSONArray("abilities").getJSONObject(0).getJSONObject("ability").getString("url");
                         String[] aabil = pabil.split("/");
-                        int abil = Integer.parseInt(aabil[aabil.length - 2]);
+                        int abil = Integer.parseInt(aabil[aabil.length - 1]);
                         abilities.add(abil);
                         String pabil2 = json.getJSONArray("abilities").getJSONObject(1).getJSONObject("ability").getString("url");
                         String[] aabil2 = pabil2.split("/");
-                        int abil2 = Integer.parseInt(aabil2[aabil2.length - 2]);
+                        int abil2 = Integer.parseInt(aabil2[aabil2.length - 1]);
                         abilities.add(abil2);
                         String hiden = json.getJSONArray("abilities").getJSONObject(2).getJSONObject("ability").getString("url");
                         String[] ahiden = hiden.split("/");
-                        hidden = Integer.parseInt(ahiden[ahiden.length - 2]);
+                        hidden = Integer.parseInt(ahiden[ahiden.length - 1]);
                     }
                     ArrayList<Integer> moves = new ArrayList<Integer>();
                     for (int i = 0; i < json.getJSONArray("moves").length(); i++) {
                         String pmove = json.getJSONArray("moves").getJSONObject(i).getJSONObject("move").getString("url");
                         String[] amove = pmove.split("/");
-                        int move = Integer.parseInt(amove[amove.length - 2]);
+                        int move = Integer.parseInt(amove[amove.length - 1]);
                         moves.add(move);
                     }
                     ArrayList<Integer> egggroup = new ArrayList<Integer>();
                     for (int i = 0; i < json2.getJSONArray("egg_groups").length(); i++) {
                         String pegg = json2.getJSONArray("egg_groups").getJSONObject(i).getString("url");
                         String[] aegg = pegg.split("/");
-                        int egg = Integer.parseInt(aegg[aegg.length - 2]);
+                        int egg = Integer.parseInt(aegg[aegg.length - 1]);
                         egggroup.add(egg);
                     }
                     ArrayList<Integer> pokedexes = new ArrayList<Integer>();
-                    for (int i = 1; i < json.getJSONArray("pokedex_numbers").length(); i++) {
-                        String pdex = json.getJSONArray("pokedex_numbers").getJSONObject(i).getJSONObject("pokedex").getString("url");
-                        String[] adex = pdex.split("/");
-                        int dex = Integer.parseInt(adex[adex.length - 2]);
-                        pokedexes.add(dex);
-                    }
+//                    System.out.println(json);
+//                    System.out.println(json.getJSONArray("pokedex_numbers").getJSONObject(0));
+
+//                    for (int i = 1; i < json.getJSONArray("pokedex_numbers").length(); i++) {
+//                        String pdex = json.getJSONArray("pokedex_numbers").getJSONObject(i).getJSONObject("pokedex").getString("url");
+//                        String[] adex = pdex.split("/");
+//                        int dex = Integer.parseInt(adex[adex.length - 1]);
+//                        pokedexes.add(dex);
+//                    }
                     Pokemon.setName(pokename);
                     Pokemon.setType1(type1);
                     Pokemon.setType2(type2);
--- src/main/java/use_case/BuildPokemonTeam/TestBuildPokemonTeam.java
+++ src/main/java/use_case/BuildPokemonTeam/TestBuildPokemonTeam.java
@@ -12,31 +12,55 @@ public class TestBuildPokemonTeam {
                 public void prepareSuccessView(BuildPokemonTeamOutputData data) {
                     System.out.println("✅ Success!");
                     System.out.println("Pokemon: " + data.getPokemon().getName());
+                    System.out.println("Type1: " + data.getPokemon().getType1());
+                    System.out.println("Type2: " + data.getPokemon().getType2());
+                    System.out.println("Stats: " + data.getPokemon().getStats());
+                    System.out.println("Abilities: " + data.getPokemon().getAbilities());
+                    System.out.println("Hidden: " + data.getPokemon().getHidden());
+                    System.out.println("Egggroup: " + data.getPokemon().getEgggroup());
+                    System.out.println("Moves: " + data.getPokemon().getMoves());
                 }
 
                 @Override
                 public void prepareFailView(String error) {
-                    System.out.println("❌ Error: " + error);
+                    System.out.println("Blame Dani for the error lmao");
                 }
             };
 
             String name = "pikachu" ;
             int type1 = 1;
             int type2 = 0;
+
             ArrayList<Integer> stats = new ArrayList<>();
+            stats.add(1);
+            stats.add(2);
+            stats.add(3);
+            stats.add(4);
+            stats.add(5);
+            stats.add(6);
+
             ArrayList<Integer> abilities = new ArrayList<>();
+            abilities.add(1);
+            abilities.add(2);
+
             int hidden = 0;
+
             ArrayList<Integer> moves = new ArrayList<>();
+            moves.add(0);
+
             ArrayList<Integer> egggroup = new ArrayList<>();
+            egggroup.add(1);
+
             ArrayList<Integer> pokedexes = new ArrayList<>();
+            egggroup.add(1);
 
 
 
 
-            Pokemon pokemon = new Pokemon(name, type1, type2, stats, abilities, hidden,  moves, egggroup, pokedexes);
+            Pokemon pikachu = new Pokemon(name, type1, type2, stats, abilities, hidden,  moves, egggroup, pokedexes);
 
             // Create the interactor
-            BuildPokemonTeamInteractor interactor = new BuildPokemonTeamInteractor(presenter, pokemon);
+            BuildPokemonTeamInteractor interactor = new BuildPokemonTeamInteractor(presenter, pikachu);
 
             // Provide some test input data
             BuildPokemonTeamInputData input = new BuildPokemonTeamInputData("pikachu");
