55dc82c5f93cc85f4ff29c7bc541cd8c8cd8285f
Amir Diba <151319977+Amir8-5@users.noreply.github.com>
2025-11-11 00:13:53 -0500

    Finished the get function that prints music events gotten from ticketmaster apoi

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,7 +1,8 @@
 package app;
 
+import data_access.DBDataAccessObject;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
     }
 }
--- src/main/java/data_access/DBDataAccessObject.java
+++ src/main/java/data_access/DBDataAccessObject.java
@@ -9,16 +9,17 @@ public class DBDataAccessObject {
     private static final String API_KEY = System.getenv("DB_KEY");
 
     public void get() {
-        System.out.println("Getting data from DB");
+        // Getting music events from TicketMaster
         final OkHttpClient client = new OkHttpClient().newBuilder().build();
         HttpUrl.Builder urlBuilder = HttpUrl.parse(BASE_URL).newBuilder();
         urlBuilder.addQueryParameter("apikey", API_KEY);
+        // Get only music events
+        urlBuilder.addQueryParameter("classificationName", "music");
         final Request request = new Request.Builder().url(urlBuilder.build()).build();
         try {
             final Response response = client.newCall(request).execute();
             System.out.println(response.code());
             if (response.isSuccessful()) {
-                System.out.println("success");
                 System.out.println(response.body().string());
             }
         } catch (IOException e) {
