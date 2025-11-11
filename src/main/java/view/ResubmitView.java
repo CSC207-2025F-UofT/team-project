59e33ae0ec2de54f6ec0feef84ef91352c19f249
Xihang Zhou <zhouxihang0728@outlook.com>
2025-11-11 11:41:14 -0500

    Build basic shape of ResubmitView

---
+++ src/main/java/view/ResubmitView.java
@@ -0,0 +1,27 @@
+package view;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ResubmitView {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JPanel resubmitPanel = new JPanel();
+            resubmitPanel.add(new JLabel("Are you sure " +
+                    "to resubmit your work? Only the last submission will be graded."));
+            JButton resubmitButton = new JButton("Reubmit");
+            resubmitButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    //TODO: Should go to upload page, select file and then data access.
+                }
+            });
+            resubmitPanel.add(resubmitButton);
+            JFrame frame = new JFrame("Resubmit");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setContentPane(resubmitPanel);
+            frame.pack();
+            frame.setVisible(true);
+        });
+    }
+}
