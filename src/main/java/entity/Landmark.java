88dbff845c489e3334043aa7d0d2ca4c327b9575
0NCEbot <danbhnguyen2006@gmail.com>
2025-11-12 19:14:25 -0500

    added landmarkId attribute back to Landmark class

--- src/main/java/entity/Landmark.java
+++ src/main/java/entity/Landmark.java
@@ -1,16 +1,22 @@
 package entity;
 
 public class Landmark {
+    private final String landmarkId;
     private final String landmarkName;
     private final Location location;
     private final LandmarkInfo landmarkInfo;
     private final int totalVisitCount;
 
-    public Landmark(String landmarkName, Location location, LandmarkInfo landmarkInfo, int totalVisitCount) {
+    public Landmark(String landmarkId, String landmarkName, Location location, LandmarkInfo landmarkInfo, int totalVisitCount) {
+        this.landmarkId = landmarkId;
         this.landmarkName = landmarkName;
         this.location = location;
         this.landmarkInfo = landmarkInfo;
         this.totalVisitCount = totalVisitCount;
+    }
+
+    public String getLandmarkId() {
+        return landmarkId;
     }
 
     public String getLandmarkName() {
