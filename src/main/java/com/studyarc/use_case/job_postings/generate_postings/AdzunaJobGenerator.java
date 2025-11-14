cf7635f021d0545236bc49519eda6b9d90c19301
Sofia Borodaenko <soph.bor06@gmail.com>
2025-11-14 18:53:52 -0500

    generate JobListing entities from api - beta

--- src/main/java/com/studyarc/use_case/job_postings/generate_postings/AdzunaJobGenerator.java
+++ src/main/java/com/studyarc/use_case/job_postings/generate_postings/AdzunaJobGenerator.java
@@ -1,5 +1,6 @@
 package com.studyarc.use_case.job_postings.generate_postings;
 
+import com.openai.core.JsonObject;
 import com.studyarc.entity.job_postings.JobListing;
 import com.studyarc.entity.job_postings.KeywordList;
 import com.studyarc.use_case.job_postings.JobPostingsInputData;
@@ -37,11 +38,13 @@ public class AdzunaJobGenerator implements JobRepository {
     }
 
     public static void getJobs(String countryCode) throws IOException {
-        String url = "https://api.adzuna.com/v1/api/jobs/" + countryCode +"/search/1?app_id=" + API_KEY + "&app_key=" + API_ID + "&results_per_page=20&what=";
+        String url = "https://api.adzuna.com/v1/api/jobs/" + countryCode +"/search/1?app_id=" + API_ID + "&app_key=" + API_KEY + "&results_per_page=20&what_or=";
 
         // adds the keywords
-        url += "Machine%20Learning Deep%20Learning Neural%20Networks AI Artificial%20Intelligence Data%20Science Reinforcement"
+        url += "machine%20learning%20ml%20deep%20learning%20neural%20networks%20data%20science%20ai%20artificial%20intelligence"
                 + "&content-type=application/json";
+
+        System.out.println(url);
 
         final Request request = new Request.Builder().url(url).build();
 
@@ -50,19 +53,44 @@ public class AdzunaJobGenerator implements JobRepository {
             final JSONObject responseBody = new JSONObject(response.body().string());
 
             System.out.println("Getting jobs from api...");
-            // gets the status of the call
-//            String status = responseBody.getString("status");
 
-            System.out.println(responseBody);
+            try {
+                if (responseBody.get("results") != null) {
 
-            // checks if its successful
-//            if (!status.equals("Not Found")) {
-//                System.out.println(responseBody);
-//            } else {
-//                System.out.println("Didn't work :(");
-//            }
+                    // gets the list of job listings from the api
+                    JSONArray jobResults = responseBody.getJSONArray("results");
+
+                    // creates the list that will be returned containing the JobListing entities
+                    List<JobListing> listings = new ArrayList<>();
+
+                    for (int i = 0; i < jobResults.length(); i++) {
+                        JSONObject job = jobResults.getJSONObject(i);
+                        JSONObject jobCompany = job.getJSONObject("company");
+                        JSONObject jobLocation = job.getJSONObject("location");
+
+//                        System.out.println(job);
+//                        System.out.println(jobCompany.get("display_name"));
+//                        System.out.println(jobLocation.get("display_name"));
+
+                        JobListing newJob = new JobListing(job.get("title").toString(), Long.parseLong(job.get("id").toString()), jobCompany.get("display_name").toString(), Double.parseDouble(job.get("salary_min").toString()), Double.parseDouble(job.get("salary_max").toString()), job.get("description").toString(), jobLocation.get("display_name").toString(), job.get("redirect_url").toString());
+                        listings.add(newJob);
+
+//                        System.out.println(newJob.getJobId() + " " + newJob.getTitle() + " " + newJob.getCompanyName() + " " + newJob.getSalaryMin() + " " + newJob.getSalaryMax() + " " + newJob.getJobLoc() + " " + newJob.getRedirectUrl() + " " + newJob.getJobDesc());
+
+                    }
+
+                    System.out.println("Finished");
+
+
+                }
+
+            } catch (Exception e) {
+                throw new IOException(e);
+            }
+
     } catch (IOException e) {
             System.out.println("Something went wrong :(");
+            System.out.println(e.getMessage());
         }
 
 
