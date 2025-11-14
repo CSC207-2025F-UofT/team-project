8bd5ccf91c07efe0eb704b7bc8c2704839bef0ec
AveryThompson7 <avery.thompson@mail.utoronto.ca>
2025-11-14 17:16:36 -0500

    PostInputData completed

--- src/main/java/use_case/post_review/PostInputData.java
+++ src/main/java/use_case/post_review/PostInputData.java
@@ -1,4 +1,33 @@
 package use_case.post_review;
 
 public class PostInputData {
+
+    private final String comment;
+    private final int rating;
+    private final String username;
+    private final String song;
+
+    public PostInputData(String comment, int rating, String username, String song) {
+        this.comment = comment;
+        this.rating = rating;
+        this.username = username;
+        this.song = song;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getSong() {
+        return song;
+    }
+
 }
