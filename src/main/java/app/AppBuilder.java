01324d145fdc4042bbe4a03c0b6435009652a4df
elena-ding <emailelenading@gmail.com>
2025-11-13 21:09:21 -0500

    implemented open team entry page use case

--- src/main/java/app/AppBuilder.java
+++ src/main/java/app/AppBuilder.java
@@ -3,7 +3,14 @@ package app;
 import interface_adapter.ViewManagerModel;
 import interface_adapter.home.HomeController;
 import interface_adapter.home.HomeViewModel;
+import interface_adapter.open_team_entry.OpenTeamEntryController;
+import interface_adapter.open_team_entry.OpenTeamEntryPresenter;
+import interface_adapter.open_team_entry.OpenTeamEntryViewModel;
+import use_case.open_team_entry.OpenTeamEntryInputBoundary;
+import use_case.open_team_entry.OpenTeamEntryInteractor;
 import view.HomePageView;
+import view.TeamEntryView;
+import view.ViewManager;
 
 import javax.swing.*;
 import java.awt.*;
@@ -12,9 +19,15 @@ public class AppBuilder {
     private final JPanel cardPanel = new JPanel();
     private final CardLayout cardLayout = new CardLayout();
     final ViewManagerModel viewManagerModel = new ViewManagerModel();
+    ViewManager viewManager = new ViewManager(cardPanel, cardLayout, viewManagerModel);
 
     private HomePageView homePageView;
     private HomeViewModel homeViewModel;
+    private TeamEntryView teamEntryView;
+    private OpenTeamEntryViewModel teamEntryViewModel;
+    private OpenTeamEntryController openTeamEntryController;
+    private OpenTeamEntryInputBoundary openTeamEntryInputBoundary;
+
 
     public AppBuilder() {
         cardPanel.setLayout(cardLayout);
@@ -27,10 +40,29 @@ public class AppBuilder {
         return this;
     }
 
-
     public AppBuilder addHomeUseCase() {
-        final HomeController homeController = new HomeController(homeViewModel);
+        final HomeController homeController = new HomeController(homeViewModel, openTeamEntryController, openTeamEntryInputBoundary);
         homePageView.setHomeController(homeController);
+        return this;
+    }
+
+    public AppBuilder addTeamEntryView() {
+        teamEntryViewModel = new OpenTeamEntryViewModel();
+        teamEntryView = new TeamEntryView(teamEntryViewModel);
+        cardPanel.add(teamEntryView, teamEntryView.getViewName());
+        return this;
+    }
+
+    public AppBuilder addOpenTeamEntryViewUseCase() {
+        final OpenTeamEntryPresenter presenter =
+                new OpenTeamEntryPresenter(viewManagerModel, teamEntryViewModel, homeViewModel);
+
+        final OpenTeamEntryInteractor interactor =
+                new OpenTeamEntryInteractor(presenter);
+
+        openTeamEntryController = new OpenTeamEntryController(interactor);
+
+        teamEntryView.setTeamEntryController(openTeamEntryController);
         return this;
     }
 
--- src/main/java/interface_adapter/open_team_entry/OpenTeamEntryController.java
+++ src/main/java/interface_adapter/open_team_entry/OpenTeamEntryController.java
@@ -1,4 +1,23 @@
 package interface_adapter.open_team_entry;
 
+import use_case.open_team_entry.OpenTeamEntryInputBoundary;
+
 public class OpenTeamEntryController {
+
+    private final OpenTeamEntryInputBoundary openTeamEntryUseCaseInteractor;
+
+    public OpenTeamEntryController(OpenTeamEntryInputBoundary openTeamEntryUseCaseInteractor) {
+        this.openTeamEntryUseCaseInteractor = openTeamEntryUseCaseInteractor;
+    }
+
+    /**
+     * Executes the Open Team Entry Page Use Case.
+     */
+    public void execute() {
+        openTeamEntryUseCaseInteractor.execute();
+    }
+
+    public void switchToHomePage() {
+        openTeamEntryUseCaseInteractor.switchToHomePage();
+    }
 }
