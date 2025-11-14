771ad0a21841aec5e4487d5d1b830c5f423bea06
adilbabri <adil.babri@gmail.com>
2025-11-14 14:36:22 -0500

    Added load game and exit game buttons to main menu panel

--- src/main/java/view/MainMenuView.java
+++ src/main/java/view/MainMenuView.java
@@ -20,6 +20,8 @@ public class MainMenuView extends JPanel implements ActionListener, PropertyChan
     private MainMenuController mainMenuController;
 
     private final JButton startGameButton;
+    private final JButton laodGameButton;
+    private final JButton exitGameButton;
 
     public MainMenuView(MainMenuViewModel mainMenuViewModel) {
         this.mainMenuViewModel = mainMenuViewModel;
@@ -27,10 +29,33 @@ public class MainMenuView extends JPanel implements ActionListener, PropertyChan
 
         final JLabel title = new JLabel(MainMenuViewModel.TITLE_LABEL);
         title.setAlignmentX(Component.CENTER_ALIGNMENT);
+        title.setFont(new Font("Arial", Font.BOLD, 48));
 
         final JPanel buttons = new JPanel();
+        buttons.setLayout(new BoxLayout(buttons, BoxLayout.Y_AXIS));
+
+        Dimension buttonsSize = new Dimension(600, 60);
+
         startGameButton = new JButton(MainMenuViewModel.START_GAME_BUTTON_LABEL);
+        startGameButton.setPreferredSize(buttonsSize);
+        startGameButton.setMaximumSize(buttonsSize);
+        startGameButton.setAlignmentX(Component.CENTER_ALIGNMENT);
         buttons.add(startGameButton);
+        buttons.add(Box.createRigidArea(new Dimension(0, 20)));
+
+        laodGameButton = new JButton(MainMenuViewModel.LAOD_GAME_BUTTON_LABEL);
+        laodGameButton.setPreferredSize(buttonsSize);
+        laodGameButton.setMaximumSize(buttonsSize);
+        laodGameButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        buttons.add(laodGameButton);
+        buttons.add(Box.createRigidArea(new Dimension(0, 20)));
+
+        exitGameButton = new JButton(MainMenuViewModel.EXIT_GAME_BUTTON_LABEL);
+        exitGameButton.setPreferredSize(buttonsSize);
+        exitGameButton.setMaximumSize(buttonsSize);
+        exitGameButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        buttons.add(exitGameButton);
+        buttons.add(Box.createRigidArea(new Dimension(0, 20)));
 
         startGameButton.addActionListener(
                 new ActionListener() {
@@ -44,6 +69,7 @@ public class MainMenuView extends JPanel implements ActionListener, PropertyChan
 
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         this.add(title);
+        this.add(Box.createVerticalStrut(100));
         this.add(buttons);
     }
 
