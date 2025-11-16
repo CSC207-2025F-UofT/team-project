bc9ba56ebea860b66742134bc713c91524ce150f
Oliver Kwun-Morfitt <oliverkwunmorfitt@gmail.com>
2025-11-16 17:54:39 -0500

    added interface to allow publishing and saving as draft

--- src/main/java/use_case/post_recipe/PostRecipeInputBoundary.java
+++ src/main/java/use_case/post_recipe/PostRecipeInputBoundary.java
@@ -1,4 +1,10 @@
 package use_case.post_recipe;
 
-public class PostRecipeInputBoundary {
+public interface PostRecipeInputBoundary {
+
+    // Publish the recipe
+    void publish(PostRecipeInputData inputData);
+
+    // Save the recipe as draft
+    void saveDraft(PostRecipeInputData inputData);
 }
