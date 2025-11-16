5edc0da81cf8127b26420e7fbff4ba91f4ef0553
Oliver Kwun-Morfitt <oliverkwunmorfitt@gmail.com>
2025-11-16 17:56:39 -0500

    added PostRecipeOutputBoundary interface

--- src/main/java/use_case/post_recipe/PostRecipeOutputBoundary.java
+++ src/main/java/use_case/post_recipe/PostRecipeOutputBoundary.java
@@ -1,4 +1,9 @@
 package use_case.post_recipe;
 
-public class PostRecipeOutputBoundary {
+public interface PostRecipeOutputBoundary {
+    void prepareSuccessView(PostDataOutputData outputData);
+    void prepareDraftSavedView(PostDataOutputData outputData);
+
+    // In case a failure occurs
+    void prepareFailedView(String errorMessage, PostRecipeInputData inputData);
 }
