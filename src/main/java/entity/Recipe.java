0df676ae1f7bf3c45e1ecfc4c72a75304b14a9ac
Oliver Kwun-Morfitt <oliverkwunmorfitt@gmail.com>
2025-11-16 17:06:24 -0500

    Updated recipe entity with getters and consitent naming

--- src/main/java/entity/Recipe.java
+++ src/main/java/entity/Recipe.java
@@ -5,23 +5,88 @@ import java.util.List;
 
 public class Recipe {
 
-    private String recipeName;
-    private int recipeId;
+    public enum Status {
+        DRAFT,
+        PUBLISHED,
+    }
+
+    private String recipeId;
+    private String authorId;
+
+    private String title;
     private String description;
     private List<Ingredient> ingredients;
+    private String imagePath;
+
     private String category;
+    private List<String> tags;
+    private List<Review> reviews;
+
+    private int views;
+    private int saves;
+
     private boolean shareable;
     private Date creationDate;
     private Date updateDate;
-    private int views;
-    private int saves;
-    private List<Review> reviews;
+    private Status status;
 
-    public String getRecipeName() {
-        return recipeName;
+    public Recipe(String recipeId,
+                  String authorId,
+                  String title,
+                  String description,
+                  List<Ingredient> ingredients,
+                  String category,
+                  List<String> tags,
+                  Status status,
+                  Date creationDate,
+                  Date updateDate,
+                  String imagePath
+                  ) {
+        this.recipeId = recipeId;
+        this.authorId = authorId;
+
+        this.title = title;
+        this.description = description;
+        this.ingredients = ingredients;
+        this.imagePath = imagePath;
+
+        this.category = category;
+        this.tags = tags;
+        this.status = status;
+        this.creationDate = creationDate;
+        this.updateDate = updateDate;
+
+        this.views = 0;
+        this.saves = 0;
+
     }
-    public void setRecipeName(String recipeName) {
-        this.recipeName = recipeName;
+
+    public String getRecipeId(){
+        return this.recipeId;
+    }
+
+    public String getAuthorId(){
+        return this.authorId;
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public String getImagePath(){
+        return this.imagePath;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+
+    public List<Ingredient> getIngredients(){
+        return this.ingredients;
+    }
+
+    public Status getStatus(){
+        return this.status;
     }
 
 }
