259efc9503df5be77e1fc5d0d68b1bc794c80c93
Jeff Liang <jefflyf2006@163.com>
2025-11-16 18:54:19 -0500

    add up some basic file for clean architecture

--- src/main/java/use_case/save_event_to_list/SaveEventToListInputBoundary.java
+++ src/main/java/use_case/save_event_to_list/SaveEventToListInputBoundary.java
@@ -1,4 +1,4 @@
 package use_case.save_event_to_list;
 
-public class SaveEventToListInputBoundary {
+public interface SaveEventToListInputBoundary {
 }
--- src/main/java/use_case/save_event_to_list/SaveEventToListOutputBoundary.java
+++ src/main/java/use_case/save_event_to_list/SaveEventToListOutputBoundary.java
@@ -1,4 +1,4 @@
 package use_case.save_event_to_list;
 
-public class SaveEventToListOutputBoundary {
+public interface SaveEventToListOutputBoundary {
 }
