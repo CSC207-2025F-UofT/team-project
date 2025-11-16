a931753b714a30f464bdcf02dbab0ed67cb27322
Hugo Ye <hugoye122@gmail.com>
2025-11-16 01:18:41 -0500

    Basic implementation of ViewManager, ViewManagerModel, ViewModel

--- src/main/java/view/ViewManager.java
+++ src/main/java/view/ViewManager.java
@@ -1,4 +1,28 @@
 package view;
 
-public class ViewManager {
+import interface_adapter.ViewManagerModel;
+
+import javax.swing.*;
+import java.awt.*;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class ViewManager implements PropertyChangeListener {
+    private final CardLayout cardLayout;
+    private final JPanel views;
+    private final ViewManagerModel viewManagerModel;
+
+    public ViewManager(JPanel views, CardLayout cardLayout, ViewManagerModel viewManagerModel) {
+        this.cardLayout = cardLayout;
+        this.views = views;
+        this.viewManagerModel = viewManagerModel;
+        this.viewManagerModel.addPropertyChangeListener(this);
+    }
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        if (evt.getPropertyName().equals("viewName")) {
+            String viewModelName = (String) evt.getNewValue();
+            cardLayout.show(views, viewModelName);
+        }
+    }
 }
