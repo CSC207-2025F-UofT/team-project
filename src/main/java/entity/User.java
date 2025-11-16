594da74f28d3c9fbd1d89759daee8a2d5ce38817
FeiyuanLiu <amandafy.liu@mail.utoronto.ca>
2025-11-16 13:52:11 -0500

    Add friend

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -7,6 +7,8 @@ public class User {
     private final String username;
     private String password;
     private final List<String> friendsUsernames = new ArrayList<>();
+    private final List<String> incomingFriendRequests = new ArrayList<>();
+    private final List<String> outgoingFriendRequests = new ArrayList<>();
 
     public User(String username, String password) {
         this.username = username;
@@ -23,9 +25,29 @@ public class User {
         return friendsUsernames;
     }
 
+    public List<String> getIncomingFriendRequests(){return incomingFriendRequests; }
+
+    public List<String> getOutgoingFriendRequests(){return outgoingFriendRequests; }
+
     public void addFriend(String friendUsername) {
         if (!friendsUsernames.contains(friendUsername)) {
             friendsUsernames.add(friendUsername);
         }
     }
+
+    public void addIncomingRequest(String fromUsername){
+        if (!incomingFriendRequests.contains(fromUsername)){
+            incomingFriendRequests.add(fromUsername);
+        }
+    }
+
+    public void removeIncomingRequest(String fromUsername){incomingFriendRequests.remove(fromUsername); }
+
+    public void addOutgoingRequest(String toUsername){
+        if (!outgoingFriendRequests.contains(toUsername)){
+            outgoingFriendRequests.add(toUsername);
+        }
+    }
+
+    public void removeOutgoingRequest(String toUsername){outgoingFriendRequests.remove(toUsername); }
 }
