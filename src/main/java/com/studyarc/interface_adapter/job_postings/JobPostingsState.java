5fd888bf1e93c423771600919e9a3c0fbb92b210
Sofia Borodaenko <soph.bor06@gmail.com>
2025-11-15 23:43:02 -0500

    populating the view models

---
+++ src/main/java/com/studyarc/interface_adapter/job_postings/JobPostingsState.java
@@ -0,0 +1,8 @@
+package com.studyarc.interface_adapter.job_postings;
+
+/**
+ * The state for the Job Postings View Model.
+ */
+public class JobPostingsState {
+
+}
--- src/main/java/com/studyarc/interface_adapter/job_postings/JobPostingsViewModel.java
+++ src/main/java/com/studyarc/interface_adapter/job_postings/JobPostingsViewModel.java
@@ -1,9 +1,21 @@
 package com.studyarc.interface_adapter.job_postings;
 
+import com.studyarc.interface_adapter.ViewModel;
+
+import java.beans.PropertyChangeSupport;
+
 /**
  * The View Model for the Job Postings View.
  */
-public class JobPostingsViewModel {
+public class JobPostingsViewModel extends ViewModel<JobPostingsState> {
 
-    public JobPostingsViewModel() {}
+    public static final String TITLE_LABEL = "Job Board";
+    public static final String LOCATION_LABEL = "Location:";
+    public static final String SALARAY_LABEL = "Salary:";
+    public static final String SORT_LABEL = "Sort:";
+
+    public JobPostingsViewModel() {
+        super("job postings");
+        setState(new JobPostingsState());
+    }
 }
