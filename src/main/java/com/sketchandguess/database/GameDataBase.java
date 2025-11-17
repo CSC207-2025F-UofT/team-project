573e6adf1ff8abf8285a250e1c99c52ee842bf5d
ponsiclius <davidparent64@gmail.com>
2025-11-17 13:47:37 -0500

    Added DeleteGame method and updated class structure to use arraylist instead of hashmap

--- src/main/java/com/sketchandguess/database/GameDataBase.java
+++ src/main/java/com/sketchandguess/database/GameDataBase.java
@@ -6,27 +6,24 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class GameDataBase implements DataBase {
     // The database storing each game.
     // The string contains the prompt used for the game, and helps us search for specific prompts.
-    public Map<Integer, GameRecord> GameData;
+    public ArrayList<GameRecord> GameData;
 
     public GameDataBase() {
-        this.GameData = new HashMap<>();
+        this.GameData = new ArrayList<>();
         try {
             List<String> lines = Files.readAllLines(Paths.get("src\\main\\resources\\games.csv"));
 
             Iterator<String> iterator = lines.iterator();
             while (iterator.hasNext()) {
                 String line = iterator.next();
-                String[] parts = line.split("|");
+                String[] parts = line.split(",");
                 GameRecord currentGame = new GameRecord(); // TODO: implement this once gameRecord constructor is implemented
-                GameData.put(Integer.parseInt(parts[0]), currentGame);
+                GameData.add(currentGame);
             }
         }
         catch (IOException ex) {
@@ -36,7 +33,7 @@ public class GameDataBase implements DataBase {
     }
 
     // overloaded version for search function
-    public GameDataBase(HashMap<Integer, GameRecord> GameData) {
+    public GameDataBase(ArrayList<GameRecord> GameData) {
         this.GameData = GameData;
     }
 
@@ -45,13 +42,23 @@ public class GameDataBase implements DataBase {
     }
 
     public GameDataBase SearchWord(String Query) {
-        HashMap<Integer, GameRecord> Matches = new HashMap<>();
-        for (GameRecord g: this.GameData.values()) {
+        ArrayList<GameRecord> Matches = new ArrayList<>();
+        for (GameRecord g: this.GameData) {
             // TODO come back to this once gamerecord is implemented
             if (g.getPrompt().contains(Query)) {
-                Matches.put(Matches.size(), g);
+                Matches.add(Matches.size(), g);
             }
         }
         return new GameDataBase(Matches);
+    }
+
+    public Boolean DeleteGame(GameRecord DeletedGame) {
+        try {
+            this.GameData.remove(DeletedGame);
+            return Boolean.TRUE;
+        } catch (Exception e) {
+            return Boolean.FALSE;
+        }
+        }
     }
 }
