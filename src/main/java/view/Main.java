85678b0a554caa9a23d9bc59ef1817c400abd52b
billchenziang33 <billziang.chen@mail.utoronto.ca>
2025-11-18 09:48:43 +0800

    change the sample user in Main code

--- src/main/java/view/Main.java
+++ src/main/java/view/Main.java
@@ -7,7 +7,7 @@ public class Main {
         SportsAPIDataAccess data = new SportsAPIDataAccess();
         // data.fetchOdds();
         data.readdata();
-        User user = new User("BillChen", 1525.00, 15, 8);
+        User user = new User("BillChen", 1525.00, 15, 8,"123456");
         new MainMenuFrame(user);
         //test_push
     }
