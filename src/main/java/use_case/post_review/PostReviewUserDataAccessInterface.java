5dbf6ae7d2cb444a0a8aa147850359940dc1bd0c
AveryThompson7 <avery.thompson@mail.utoronto.ca>
2025-11-18 17:36:56 -0500

    Created interactor for PostReview usecase

---
+++ src/main/java/use_case/post_review/PostReviewUserDataAccessInterface.java
@@ -0,0 +1,11 @@
+package use_case.post_review;
+
+import entity.Review;
+
+public interface PostReviewUserDataAccessInterface {
+
+    public void addReview(Review review, String Username);
+
+
+}
+
