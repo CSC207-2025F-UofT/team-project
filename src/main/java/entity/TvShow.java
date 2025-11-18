4ba46fe057a90a7f904b77fc3b0d9c13d29b8363
Eileen1222 <yihe.xiao@mail.utoronto.ca>
2025-11-17 21:36:04 -0500

    add name parameter to the constructor of the 2 entity which keep the same as the media class

--- src/main/java/entity/TvShow.java
+++ src/main/java/entity/TvShow.java
@@ -4,8 +4,8 @@ public class TvShow extends Media{
     public final Episode[][] episodes;
     public final int seasons;
 
-    public TvShow(int referenceNumber, int[] genreIDs, Episode[][] episodes, int seasons, Episode[][] episodes1, int seasonNumber) {
-        super(referenceNumber, genreIDs);
+    public TvShow(String name, int referenceNumber, int[] genreIDs, Episode[][] episodes, int seasons, Episode[][] episodes1, int seasonNumber) {
+        super(name, referenceNumber, genreIDs);
         this.episodes = episodes1;
         this.seasons = seasonNumber;
     }
