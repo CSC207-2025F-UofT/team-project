1176b7e0f5db18bdb814077c07a99059a074d4d6
Your Name <william.ignasiak@gmail.com>
2025-11-17 21:41:25 -0500

    login view implemented with a test

--- src/main/java/view/Main.java
+++ src/main/java/view/Main.java
@@ -5,7 +5,7 @@ import data_access.SportsAPIDataAccess;
 public class Main {
     public static void main(String[] args) {
         SportsAPIDataAccess data = new SportsAPIDataAccess();
-        User user = new User("BillChen", 1525.00, 15, 8);
+        User user = new User("BillChen", 1525.00, 15, 8, "1234");
         new MainMenuFrame(user);
         //test_push
     }
