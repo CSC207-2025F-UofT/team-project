27fa223dd4d32fdf66bffc4e5034581230d1d3ca
shidafuyang-alt <shidafuyang@gmail.com>
2025-11-18 15:26:04 -0500

    update interface_adapter for send-message

---
+++ src/main/java/interface_adapter/messaging/send_m/SendMessagePresenter.java
@@ -0,0 +1,41 @@
+package interface_adapter.messaging.send_m;
+
+import interface_adapter.ViewManagerModel;
+import interface_adapter.messaging.send_m.chat.ChatState;
+import interface_adapter.messaging.send_m.chat.ChatViewModel;
+import use_case.messaging.ChatMessageDto;
+import use_case.messaging.send_m.SendMessageOutputBoundary;
+import use_case.messaging.send_m.SendMessageOutputData;
+
+public class SendMessagePresenter implements SendMessageOutputBoundary {
+
+    private final ChatViewModel chatViewModel;
+    private final ViewManagerModel viewManagerModel;
+
+    public SendMessagePresenter(ChatViewModel chatViewModel,
+                                ViewManagerModel viewManagerModel) {
+        this.chatViewModel = chatViewModel;
+        this.viewManagerModel = viewManagerModel;
+    }
+
+    @Override
+    public void prepareSuccessView(SendMessageOutputData outputData) {
+        ChatState state = chatViewModel.getState();
+        ChatMessageDto dto = outputData.getMessage();
+
+        state.addMessage(dto);
+        state.setError(null);
+
+        chatViewModel.firePropertyChange();
+
+        viewManagerModel.setState(chatViewModel.getViewName());
+        viewManagerModel.firePropertyChange();
+    }
+
+    @Override
+    public void prepareFailView(String errorMessage) {
+        ChatState state = chatViewModel.getState();
+        state.setError(errorMessage);
+        chatViewModel.firePropertyChange();
+    }
+}
