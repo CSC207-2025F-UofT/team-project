d86ddf422ac1e381db5a20e508973a95d3ba90d0
DarrenChailand <darrenchailand2@gmail.com>
2025-11-19 12:52:55 -0500

    modify HomescreenViewModel so it implements ViewModel

--- src/main/java/interface_adapter/homescreen/HomescreenViewModel.java
+++ src/main/java/interface_adapter/homescreen/HomescreenViewModel.java
@@ -1,32 +1,11 @@
 package interface_adapter.homescreen;
 
-import java.beans.PropertyChangeListener;
-import java.beans.PropertyChangeSupport;
+import interface_adapter.ViewModel;
 
-public class HomescreenViewModel {
-    private final String viewName = "homescreen";
-    private HomescreenState state = new HomescreenState();
-    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+public class HomescreenViewModel extends ViewModel<HomescreenState> {
 
-    public HomescreenViewModel() {}
-
-    public void setState(HomescreenState state) {
-        this.state = state;
-    }
-
-    public HomescreenState getState() {
-        return state;
-    }
-
-    public String getViewName() {
-        return viewName;
-    }
-
-    public void firePropertyChanged() {
-        support.firePropertyChange("state", null, this.state);
-    }
-
-    public void addPropertyChangeListener(PropertyChangeListener listener) {
-        support.addPropertyChangeListener(listener);
+    public HomescreenViewModel() {
+        super("homescreen");
+        setState(new HomescreenState());
     }
 }
