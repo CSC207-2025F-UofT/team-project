068f6c0a925653c8805d475ab88f376a365b2517
AquaDummy <awesomejay098@gmail.com>
2025-11-19 17:08:36 -0500

    Added restaurant name, address, and menu items in the Menu View.

--- src/main/java/interface_adaptor/Menu/MenuState.java
+++ src/main/java/interface_adaptor/Menu/MenuState.java
@@ -1,11 +1,16 @@
 package interface_adaptor.Menu;
 
+import entity.MenuItem;
 import entity.Restaurant;
+
+import java.util.ArrayList;
 
 public class MenuState {
     private String name = "";
     private String restaurantId;
     private float rating = 0;
+    private String address = "";
+    private ArrayList<MenuItem> menuList;
 
     public String getName(){
         return this.name;
@@ -16,14 +21,26 @@ public class MenuState {
     public float getRating(){
         return this.rating;
     }
+    public String getAddress(){
+        return this.address;
+    }
+    public ArrayList<MenuItem> getMenuList(){
+        return this.menuList;
+    }
+
     public void setName(String name){
         this.name = name;
     }
     public void setRestaurant(String rest){
         this.restaurantId = rest;
     }
-    public void setRating(float rating){
+    public void setRating(float rating) {
         this.rating = rating;
     }
-
+    public void setAddress(String address){
+        this.address = address;
+    }
+    public void setMenuList(ArrayList<MenuItem> menuList){
+        this.menuList = menuList;
+    }
 }
