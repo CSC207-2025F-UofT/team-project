e4782a52ccf4d69e146684aca44854a648f446ed
Hugo Ye <hugoye122@gmail.com>
2025-11-20 11:24:15 -0500

    Implemented `LoginView` and `SignupView` with controllers, view models, and input listeners. Enhanced UI behavior and error handling through property change listeners.

--- src/main/java/view/LoginView.java
+++ src/main/java/view/LoginView.java
@@ -1,14 +1,131 @@
 package view;
 
+import interface_adapter.login.LoginController;
+import interface_adapter.login.LoginState;
+import interface_adapter.login.LoginViewModel;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
-public class LoginView extends JPanel implements ActionListener {
-    private final String viewName = "login view";
+public class LoginView extends JPanel implements ActionListener, PropertyChangeListener {
+    private final String viewName = "log in";
+    private final LoginViewModel loginViewModel;
+    private final LoginController loginController;
+
+    private final JTextField usernameField = new JTextField(15);
+    private final JPasswordField passwordField = new JPasswordField(15);
+
+    private final JButton login;
+    private final JButton signUp;
+
+    public LoginView(LoginViewModel loginViewModel, LoginController loginController) {
+        this.loginViewModel = loginViewModel;
+        this.loginController = loginController;
+        loginViewModel.addPropertyChangeListener(this);
+
+        JLabel title = new JLabel("Log in");
+        title.setAlignmentX(CENTER_ALIGNMENT);
+
+        JPanel usernamePanel = new JPanel();
+        usernamePanel.add(new JLabel("Username:"));
+        usernamePanel.add(usernameField);
+
+        JPanel passwordPanel = new JPanel();
+        passwordPanel.add(new JLabel("Password:"));
+        passwordPanel.add(passwordField);
+
+        JPanel buttonPanel = new JPanel();
+        login = new JButton("Log in");
+        buttonPanel.add(login);
+        signUp = new JButton("Cancel");
+        buttonPanel.add(signUp);
+
+        login.addActionListener(
+                new ActionListener() {
+                    public void actionPerformed(ActionEvent evt) {
+                        if (evt.getSource().equals(login)) {
+                            LoginState currentState = loginViewModel.getState();
+
+                            loginController.execute(
+                                    currentState.getUsername(),
+                                    currentState.getPassword()
+                            );
+                        }
+                    }
+                }
+        );
+
+        signUp.addActionListener(
+                new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        // I need this to direct to the signup view
+                    }
+                }
+        );
+
+
+        usernameField.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                LoginState currentState = loginViewModel.getState();
+                currentState.setUsername(usernameField.getText() + e.getKeyChar());
+                loginViewModel.setState(currentState);
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+
+            }
+        });
+        passwordField.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                LoginState currentState = loginViewModel.getState();
+                currentState.setPassword(new String(passwordField.getPassword()) + e.getKeyChar());
+                loginViewModel.setState(currentState);
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+
+            }
+        });
+
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        this.add(title);
+        this.add(usernamePanel);
+        this.add(passwordPanel);
+        this.add(buttonPanel);
+    }
+
+
+
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("This doesnt really do anything yet");
+    }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
-
+    public void propertyChange(PropertyChangeEvent evt) {
+        LoginState state = (LoginState) evt.getNewValue();
+        if (state.getErrorMessage() != null) {
+            JOptionPane.showMessageDialog(this, state.getErrorMessage());
+        }
     }
 }
--- src/main/java/view/SignupView.java
+++ src/main/java/view/SignupView.java
@@ -1,13 +1,158 @@
 package view;
 
+import interface_adapter.signup.SignupController;
+import interface_adapter.signup.SignupState;
+import interface_adapter.signup.SignupViewModel;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
-public class SignupView extends JPanel implements PropertyChangeListener {
+public class SignupView extends JPanel implements ActionListener, PropertyChangeListener {
+
+    private final String viewName = "sign up";
+
+    private final SignupViewModel signupViewModel;
+    private final SignupController signupController;
+
+    private final JTextField usernameInputField = new JTextField(15);
+    private final JPasswordField passwordInputField = new JPasswordField(15);
+    private final JPasswordField repeatPasswordInputField = new JPasswordField(15);
+
+    private final JButton signUp;
+    private final JButton cancel;
+
+    public SignupView(SignupViewModel signupViewModel, SignupController signupController) {
+        this.signupViewModel = signupViewModel;
+        this.signupController = signupController;
+        signupViewModel.addPropertyChangeListener(this);
+
+        JLabel title = new JLabel("Sign up");
+        title.setAlignmentX(CENTER_ALIGNMENT);
+
+        JPanel usernamePanel = new JPanel();
+        usernamePanel.add(new JLabel("Username:"));
+        usernamePanel.add(usernameInputField);
+
+        JPanel passwordPanel = new JPanel();
+        passwordPanel.add(new JLabel("Password:"));
+        passwordPanel.add(passwordInputField);
+
+        JPanel repeatPasswordPanel = new JPanel();
+        repeatPasswordPanel.add(new JLabel("Repeat password:"));
+        repeatPasswordPanel.add(repeatPasswordInputField);
+
+        JPanel buttonPanel = new JPanel();
+        signUp = new JButton("Sign up");
+        buttonPanel.add(signUp);
+        cancel = new JButton("Cancel");
+        buttonPanel.add(cancel);
+
+        signUp.addActionListener(
+                new ActionListener() {
+                    public void actionPerformed(ActionEvent evt) {
+                        if (evt.getSource().equals(signUp)) {
+                            SignupState currentState = signupViewModel.getState();
+                            signupController.execute(
+                                    currentState.getUsername(),
+                                    currentState.getPassword(),
+                                    currentState.getRepeatPassword()
+                            );
+                        }
+                    }
+                }
+        );
+
+        cancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Switch to login view
+            }
+        });
+
+        addUsernameListener();
+        addPasswordListener();
+        addRepeatPasswordListener();
+
+
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        this.add(title);
+        this.add(usernamePanel);
+        this.add(passwordPanel);
+        this.add(repeatPasswordPanel);
+        this.add(buttonPanel);
+    }
+
+    private void addUsernameListener(){
+        usernameInputField.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                SignupState currentState = signupViewModel.getState();
+                String text = usernameInputField.getText() + e.getKeyChar();
+                currentState.setUsername(text);
+                signupViewModel.setState(currentState);
+            }
+            @Override
+            public void keyPressed(KeyEvent e) {
+            }
+            @Override
+            public void keyReleased(KeyEvent e) {
+
+            }
+                                             }
+        );
+    }
+
+    private void addPasswordListener(){
+        passwordInputField.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                SignupState currentState = signupViewModel.getState();
+                currentState.setPassword(new String(passwordInputField.getPassword()) + e.getKeyChar());
+                signupViewModel.setState(currentState);
+            }
+            @Override
+            public void keyPressed(KeyEvent e) {
+            }
+            @Override
+            public void keyReleased(KeyEvent e) {
+
+            }
+        });
+    }
+
+    private void addRepeatPasswordListener(){
+        repeatPasswordInputField.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                SignupState currentState = signupViewModel.getState();
+                currentState.setRepeatPassword(new String(repeatPasswordInputField.getPassword()) + e.getKeyChar());
+                signupViewModel.setState(currentState);
+            }
+            @Override
+            public void keyPressed(KeyEvent e) {
+            }
+            @Override
+            public void keyReleased(KeyEvent e) {
+
+            }
+        });
+    }
 
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
+        SignupState state = (SignupState) evt.getNewValue();
+        if (state.getErrorMessage() != null) {
+            JOptionPane.showMessageDialog(this, state.getErrorMessage());
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
 
     }
 }
